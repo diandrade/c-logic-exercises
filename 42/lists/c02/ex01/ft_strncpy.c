3b7bb5634d5a05f361233f86da02c92853e7d875
diandrade <contato.andradediego@gmail.com>
2025-06-09 12:13:27 -0300

    ex01 da C02 concluído.

---
+++ 42/lists/c02/ex01/ft_strncpy.c
@@ -0,0 +1,56 @@
+#include <unistd.h>
+
+void	ft_putstr(char *src)
+{
+	int	i;
+
+	i = 0;
+	while(src[i] != '\0')
+	{
+		write(1, &src[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int	ft_strlen(char *src)
+{
+	int	i;
+
+	i = 0;
+	while(src[i] != '\0')
+	{
+		i++;
+	}
+	return i;
+}
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	int	i;
+
+	i = 0;
+	while(src[i] != '\0' && n)
+	{
+		dest[i] = src[i];
+		i++;
+		n--;
+	}
+
+	if(n != 0)
+	{
+		dest[i] = '\0';		
+	}
+	return dest;
+}
+
+int	main(void)
+{
+	char	src[] = "String";
+	char	dest[ft_strlen(src)];
+
+	ft_strncpy(dest, src, 5);
+	ft_putstr(dest);
+
+	return (0);
+}
